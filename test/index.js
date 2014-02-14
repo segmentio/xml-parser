f22a23da033a1a5f9d69f92da512c7f0a6e70e81
TJ Holowaychuk <tj@vision-media.ca>
2014-02-14 12:35:11 -0800

    add more tests

--- test/index.js
+++ test/index.js
@@ -17,3 +17,48 @@ it('should support declarations', function(){
     root: undefined
   })
 })
+
+it('should support tags', function(){
+  var node = parse('<foo></foo>');
+  node.root.should.eql({
+    name: 'foo',
+    attributes: {},
+    children: [],
+    content: ''
+  });
+})
+
+it('should support tags with text', function(){
+  var node = parse('<foo>hello world</foo>');
+  node.root.should.eql({
+    name: 'foo',
+    attributes: {},
+    children: [],
+    content: 'hello world'
+  });
+})
+
+it('should support nested tags', function(){
+  var node = parse('<a><b><c>hello</c></b></a>');
+  node.root.should.eql({
+    "name": "a",
+    "attributes": {},
+    "children": [
+      {
+        "name": "b",
+        "attributes": {},
+        "children": [
+          {
+            "name": "c",
+            "attributes": {},
+            "children": [],
+            "content": "hello"
+          }
+        ],
+        "content": ""
+      }
+    ],
+    "content": ""
+  })
+})
+
