8abae87b138fad1960bc29d7067e5d5edad50ccc
TJ Holowaychuk <tj@vision-media.ca>
2014-02-14 12:38:46 -0800

    make examples/index.js generic

--- examples/index.js
+++ examples/index.js
@@ -1,7 +1,10 @@
 
+var path = process.argv[2];
+if (!path) throw new Error('path required');
+
 var fs = require('fs');
 var parse = require('..');
-var xml = fs.readFileSync('examples/developerforce.xml', 'utf8');
+var xml = fs.readFileSync(path, 'utf8');
 var inspect = require('util').inspect;
 
 var obj = parse(xml);
